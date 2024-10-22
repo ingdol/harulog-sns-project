8fc5acdaa3fd00ffe71f48634489d8598b79cc2c
ingdol <dmswl3073@naver.com>
2024-10-22 23:53:03 +0900

    Refactor: 폴더 구조를 authorized와 unauthorized로 분리 #1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import MainLayout from "@/components/layouts/main-layout";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <MainLayout>{children}</MainLayout>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
