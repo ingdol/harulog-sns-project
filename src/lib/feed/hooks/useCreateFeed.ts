81e5b63d14c0e99f60332345adc274926695d206
ingdol <dmswl3073@naver.com>
2024-10-31 15:58:40 +0900

    Feat: 게시글 작성 후 리스트 자동 업데이트 기능 추가 #5

--- src/lib/feed/hooks/useCreateFeed.ts
+++ src/lib/feed/hooks/useCreateFeed.ts
@@ -1,12 +1,21 @@
 import { createFeed } from "@/actions/feed-action";
 import { useMutation } from "@tanstack/react-query";
 import { IFeed, NewFeedDTO } from "../types";
+import { queryClient } from "@/providers/TanstackQueryClientProvider";
+import { FEED_KEY } from "../key";
+import { useRouter } from "next/navigation";
 
 export const useCreateFeed = () => {
+  const router = useRouter();
+
   return useMutation<IFeed, Error, NewFeedDTO>({
     mutationFn: createFeed,
     onSuccess: () => {
       console.log("feed 생성 성공");
+      queryClient.invalidateQueries({
+        queryKey: [FEED_KEY],
+      });
+      router.back();
     },
     onError: (error: Error) => {
       console.log(error);
