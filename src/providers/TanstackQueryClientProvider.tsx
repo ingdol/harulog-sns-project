165e2c591751035b32ad2c325bea409425807587
ingdol <dmswl3073@naver.com>
2024-11-06 00:38:09 +0900

    Chore: react query 권장 초기 설정으로 TanstackQueryClientProvider 적용 - Next.js Server Components 권장 설정 방식 준수 - SSR 최적화를 위해 기본 옵션인 staleTime 설정
    Relate to: #4

--- src/providers/TanstackQueryClientProvider.tsx
+++ src/providers/TanstackQueryClientProvider.tsx
@@ -3,18 +3,37 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const TanstackQueryClientProvider = ({
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function TanstackQueryClientProvider({
   children,
 }: {
   children: React.ReactNode;
-}) => {
-  const queryClient = new QueryClient();
+}) {
+  const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       {children}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
-};
-
-export default TanstackQueryClientProvider;
+}
