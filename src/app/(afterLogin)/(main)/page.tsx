bd05136ef68d0aca3c412bfb007bb7a14f2ef56b
eunji Shin <65396646+ingdol@users.noreply.github.com>
2024-12-01 20:15:07 +0900

    prefetch 안한 버전

--- src/app/(afterLogin)/(main)/page.tsx
+++ src/app/(afterLogin)/(main)/page.tsx
@@ -7,30 +7,30 @@ import {
 } from "@tanstack/react-query";
 
 export default async function HomePage() {
-  const queryClient = new QueryClient();
+  // const queryClient = new QueryClient();
 
-  await queryClient.prefetchInfiniteQuery({
-    queryKey: [FEED_KEY],
-    queryFn: async () => {
-      const baseUrl =
-        process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
-      const res = await fetch(`${baseUrl}/api/feed`, {
-        cache: "no-store",
-      });
-      if (!res.ok) throw new Error("Failed to fetch initial posts");
-      return await res.json();
-    },
-    initialPageParam: 1,
-    staleTime: 60 * 1000,
-  });
+  // await queryClient.prefetchInfiniteQuery({
+  //   queryKey: [FEED_KEY],
+  //   queryFn: async () => {
+  //     const baseUrl =
+  //       process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  //     const res = await fetch(`${baseUrl}/api/feed`, {
+  //       cache: "no-store",
+  //     });
+  //     if (!res.ok) throw new Error("Failed to fetch initial posts");
+  //     return await res.json();
+  //   },
+  //   initialPageParam: 1,
+  //   staleTime: 60 * 1000,
+  // });
 
-  const dehydratedState = dehydrate(queryClient);
+  // const dehydratedState = dehydrate(queryClient);
   return (
     <div>
       <FeedTopMenu />
-      <HydrationBoundary state={dehydratedState}>
-        <FeedList />
-      </HydrationBoundary>
+      {/* <HydrationBoundary state={dehydratedState}> */}
+      <FeedList />
+      {/* </HydrationBoundary> */}
     </div>
   );
 }
